f3c6c17b99185795bd2f2ead6da70837d1c7195c
Vishrut19 <vishrutagarwalla19@gmail.com>
2023-12-16 21:40:23 +0530

    Added Apollo Client and useQuery Hook

--- packages/client/src/components/Home.jsx
+++ packages/client/src/components/Home.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_ALL_QUOTES } from "../gqloperations/queries.js";
 
-function Home(props) {
+function Home() {
+  const { loading, error, data } = useQuery(GET_ALL_QUOTES);
+  if (loading) return <h1>Loading...</h1>;
+  if (error) {
+    console.log(error.message);
+  }
   return (
     <div className="container">
-      <blockquote>
-        <h6>if it works don't touch it</h6>
-        <p className="right-align">~ Vishrut</p>
-      </blockquote>
-      <blockquote>
-        <h6>if it works don't touch it</h6>
-        <p className="right-align">~ Vishrut</p>
-      </blockquote>
+      {data.quotes.map((quote, index) => {
+        return (
+          <blockquote key={index}>
+            <h6>{quote.name}</h6>
+            <p className="right-align">~ {quote.by.firstName}</p>
+          </blockquote>
+        );
+      })}
     </div>
   );
 }
